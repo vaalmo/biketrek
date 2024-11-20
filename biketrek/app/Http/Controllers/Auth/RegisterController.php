0b7e0fd3bf3c5dcf4b615fe40df9ab00f184e174
vaalmo <valentinamoralesvillada17@outlook.com>
2024-11-19 22:54:39 -0500

    (fix): fixed customuser model usage

--- biketrek/app/Http/Controllers/Auth/RegisterController.php
+++ biketrek/app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\CustomUser;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -50,7 +50,9 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:custom_users'],
+            'address' => ['required', 'string', 'max:255'],
+            'phoneNumber' => ['required', 'integer', 'min:10'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -59,14 +61,17 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \App\Models\CustomUser
      */
     protected function create(array $data)
     {
-        return User::create([
+        return CustomUser::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'address' => $data['address'],
+            'phoneNumber' => $data['phoneNumber'],
             'password' => Hash::make($data['password']),
+            'balance' => 5000, 
         ]);
     }
 }
