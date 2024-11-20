f601ff7941c83bda2fad5ff0b85231efb33b729f
vaalmo <valentinamoralesvillada17@outlook.com>
2024-11-19 22:54:55 -0500

    (update): added middleware for admin access

---
+++ biketrek/app/Http/Middleware/AdminAuthMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role == 'admin') { 
+            return $next($request); 
+        } else { 
+            return redirect()->route('home.index'); 
+        }
+    }
+}
