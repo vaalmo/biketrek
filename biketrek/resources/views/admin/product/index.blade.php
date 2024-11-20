e476929bf0362ea1be6d120217ee425adb7c2dca
vaalmo <valentinamoralesvillada17@outlook.com>
2024-11-19 21:10:33 -0500

    (update): admin product index with edit and delete buttons

--- biketrek/resources/views/admin/product/index.blade.php
+++ biketrek/resources/views/admin/product/index.blade.php
@@ -67,21 +67,25 @@
   </div>
   <div class="card-body">
     <table class="table table-bordered table-striped">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Edit</th>
-          <th scope="col">Delete</th>
-        </tr>
-      </thead>
-      <tbody>
+    <tbody>
         @foreach ($viewData["products"] as $product)
         <tr>
           <td>{{ $product->getId() }}</td>
           <td>{{ $product->getName() }}</td>
-          <td>Edit</td>
-          <td>Delete</td>
+          <td>
+            <a class="btn btn-primary" href="{{route('admin.product.edit', ['id'=> $product->getId()])}}">
+              <i class="bi-pencil"></i>
+            </a>
+          </td>
+          <td>
+            <form action="{{ route('admin.product.delete', $product->getId())}}" method="POST">
+              @csrf
+              @method('DELETE')
+              <button class="btn btn-danger">
+                <i class="bi-trash"></i>
+              </button>
+            </form>
+          </td>
         </tr>
         @endforeach
       </tbody>
